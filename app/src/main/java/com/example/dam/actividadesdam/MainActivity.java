a06b7a7f81c834775962a5e588ff9dbabff33cbb
daiannicobitz <daianbitz98@gmail.com>
2021-09-01 19:01:08 -0300

    Se modificó la ubicación de los elementos con linearlayout horizontales y se hizo la verificacion de la visibilidad del seekbar

--- app/src/main/java/com/example/dam/actividadesdam/MainActivity.java
+++ app/src/main/java/com/example/dam/actividadesdam/MainActivity.java
@@ -2,14 +2,43 @@ package com.example.dam.actividadesdam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String[] categorias = {"Indumentaria", "Electrónica", "Entretenimiento", "Jardin", "Vehículos", "Juguetes"};
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,categorias);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+
+        Switch switch_descuento = findViewById(R.id.switch1);
+        LinearLayout layout_seekbar_descuento = findViewById(R.id.layout_descuento);
+
+        switch_descuento.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener () {
+        @Override
+        public void onCheckedChanged (CompoundButton buttonView , boolean isChecked ){
+
+            if(isChecked){
+                layout_seekbar_descuento.setVisibility(View.VISIBLE);
+            }else{
+                layout_seekbar_descuento.setVisibility(View.GONE);
+            }
+        }});
+
     }
 
-    //prueba
 }
