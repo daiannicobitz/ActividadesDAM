f788cc3554c2a14fb9b7aae86f61058f34d24c75
Fachurraxx <ramagrippo@gmail.com>
2021-09-02 16:07:37 -0300

    Se declararon los EDITTEXT al principio, se validaron los datos obligatorios del punto 1. Se valido el precio del punto 3. Falta ordenar lo de los Toast.maketext

--- app/src/main/java/com/example/dam/actividadesdam/MainActivity.java
+++ app/src/main/java/com/example/dam/actividadesdam/MainActivity.java
@@ -14,7 +14,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText_retiro, editText_titulo;
+    EditText editText_titulo= findViewById(R.id.editTxtTitulo);
+    EditText editText_precio = findViewById(R.id.editTextPrecio);
+    EditText editText_email = findViewById(R.id.editTextEmail);
+    EditText editText_descripcion = findViewById(R.id.editTextDescripcion);
+    EditText editText_retiro = findViewById(R.id.editTextDireccionRetiro);
+    CheckBox checkbox_retiro = findViewById(R.id.checkBoxRetiro);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +53,6 @@ public class MainActivity extends AppCompatActivity {
 
         //Visibilidad del editText direccion de retiro
 
-        CheckBox checkbox_retiro = findViewById(R.id.checkBoxRetiro);
-
-        editText_retiro = findViewById(R.id.editTextDireccionRetiro);
-
         checkbox_retiro.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener () {
             @Override
             public void onCheckedChanged (CompoundButton buttonView , boolean isChecked ){
@@ -65,21 +66,39 @@ public class MainActivity extends AppCompatActivity {
 
         //Validacion de datos obligatorios
 
-        editText_titulo= findViewById(R.id.editTxtTitulo);
-
 
     }
 
     //Validar elementos al presionar el boton PUBLICAR
     public void Publicar(View view){
 
-        String titulo=editText_titulo.getText().toString();
+        String titulo = editText_titulo.getText().toString();
+        String precioAux = editText_precio.getText().toString();
+        String direccionRetiro = editText_retiro.getText().toString();
+        String email = editText_email.getText().toString();
 
         if(titulo.isEmpty()){
             Toast.makeText(this,"Debes ingresar un titulo",Toast.LENGTH_LONG).show(); //Mensaje que avisa al usuario.
         }
 
+        if(precioAux.isEmpty()){
+            Toast.makeText(this, "Debe ingresar un precio",Toast.LENGTH_LONG).show();
+        }else{
+            Float precio = Float.parseFloat(precioAux);
+            if(precio<=0){
+                Toast.makeText(this, "El precio debe ser positivo",Toast.LENGTH_LONG).show();
+            }
+        }
 
+        //Categoria esta definido automaticamente indumentaria
+
+        if(checkbox_retiro.isChecked() && direccionRetiro.isEmpty()){
+            Toast.makeText(this, "Debe ingresar una direccion de retiro",Toast.LENGTH_LONG).show();
+        }
+
+        if(!email.isEmpty()){
+
+        }
 
     }
 
