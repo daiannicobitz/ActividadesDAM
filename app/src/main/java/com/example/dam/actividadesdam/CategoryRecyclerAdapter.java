d95a125d0b3d990b39e83480f31d3ff73b8e4161
Fachurraxx <ramagrippo@gmail.com>
2021-09-22 01:12:25 -0300

    Implementacion de metodos principales para recycler

---
+++ app/src/main/java/com/example/dam/actividadesdam/CategoryRecyclerAdapter.java
@@ -0,0 +1,42 @@
+package com.example.dam.actividadesdam;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class CategoryRecyclerAdapter extends RecyclerView.Adapter<CategoryViewHolder> {
+
+
+    private List<Category> listaCategorias;
+
+    public CategoryRecyclerAdapter(List<Category> lista){
+        listaCategorias = lista;
+    }
+
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(ViewGroup viewGroup, int tipo) {
+//        View v = LayoutInflater.from(viewGroup.getContext())
+//                .inflate(R.layout.cat_fila, parent, false);
+//        //....
+//        CategoryViewHolder vh = new CategoryViewHolder(v);
+//        return vh;
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(CategoryViewHolder categoryViewHolder, int i) {
+        categoryViewHolder.id.setTag(i);
+        categoryViewHolder.nombreCat.setTag(i);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaCategorias.size();
+    }
+}
--- app/src/main/java/com/example/dam/actividadesdam/CategoryViewHolder.java
+++ app/src/main/java/com/example/dam/actividadesdam/CategoryViewHolder.java
@@ -3,11 +3,18 @@ package com.example.dam.actividadesdam;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 public class CategoryViewHolder extends RecyclerView.ViewHolder{
 
+    TextView id;
+    TextView nombreCat;
 
-    public CategoryViewHolder(@NonNull View itemView) {
-        super(itemView);
+
+
+    public CategoryViewHolder(View v) {
+        super(v);
+        //id = v.findViewById(R.id.idcat);
+        //nombreCat = v.findViewById(nombrecat);
     }
 }
