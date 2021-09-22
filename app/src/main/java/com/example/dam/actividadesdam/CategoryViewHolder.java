566314118a48c3d100a755245ecb25ac96b5dc93
Fachurraxx <ramagrippo@gmail.com>
2021-09-22 00:28:34 -0300

    Creacion de clases y archivos

---
+++ app/src/main/java/com/example/dam/actividadesdam/CategoryViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.dam.actividadesdam;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class CategoryViewHolder extends RecyclerView.ViewHolder{
+
+
+    public CategoryViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
